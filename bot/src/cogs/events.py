20c736189b8ff1998c83958fd5e5a3e488238bfc
ThePhoDit <42234092+ThePhoDit@users.noreply.github.com>
2023-08-17 00:23:46 +0200

    Modal and more inputs

--- bot/src/cogs/events.py
+++ bot/src/cogs/events.py
@@ -1,11 +1,52 @@
 import logging
 
-import constants
+import src.constants as constants
 import discord
-from discord import Embed
+from discord import Embed, InputTextStyle, Interaction
 from discord.ext import commands
+from discord.ui import Modal, InputText
 
 logger = logging.getLogger('BOT')
+
+
+class MyModal(Modal):
+    def __init__(self, link: str, category: str, priority: int, note: str, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        if note == "Sin nota.":
+            note = ""
+
+        self.add_item(InputText(label="Categoría", style=InputTextStyle.short, value=category))
+        self.add_item(InputText(label="Prioridad", style=InputTextStyle.short, value=str(priority)))
+        self.add_item(InputText(label="Nota del Usuario", style=InputTextStyle.long, value=note, required=False))
+        self.add_item(InputText(label="Nota del Revisor", style=InputTextStyle.long, value="", required=False))
+
+        self._link = link
+        self._original_category = category
+        self._original_priority = priority
+
+    async def callback(self, interaction: Interaction):
+        category = self.children[0].value
+        priority = int(self.children[1].value)
+        user_note = self.children[2].value
+        reviewer_note = self.children[3].value
+
+        if category not in constants.MALICIOUS_CATEGORIES:
+            category = self._original_category
+
+        if 0 > priority > 10:
+            priority = self._original_priority
+
+        # API CALL
+        print(self._link, category, priority, user_note, reviewer_note, sep="\n")
+        embed = Embed(
+            color=discord.Color.green(),
+            title="Gracias por la valoración",
+            description="Enlace aprobado."
+        )
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        # TODO: It does not delete it
+        await interaction.delete_original_response()
 
 
 class Events(commands.Cog):
@@ -20,7 +61,7 @@ class Events(commands.Cog):
             )
         )
 
-        logger.info("Bot en marcha!")
+        logger.info("Gateway connected!")
 
     @commands.Cog.listener()
     async def on_interaction(self, interaction: discord.Interaction):
@@ -32,24 +73,27 @@ class Events(commands.Cog):
             embed = None
 
             # TODO: Api calls
-            match interaction.custom_id:
-                case "approved-link":
-                    embed = Embed(
-                        color=discord.Color.green(),
-                        title="Gracias por la valoración",
-                        description="Enlace aprobado."
-                    )
-                    # Api call when approved
-                case "rejected-link":
-                    embed = Embed(
-                        color=discord.Color.red(),
-                        title="Gracias por la valoración",
-                        description="Enlace rechazado."
-                    )
-                    # Api call when rejected
+            if interaction.custom_id == "rejected-link":
+                embed = Embed(
+                    color=discord.Color.red(),
+                    title="Gracias por la valoración",
+                    description="Enlace rechazado."
+                )
 
-            await interaction.response.send_message(embed=embed, ephemeral=True)
-            await interaction.delete_original_message()
+                # Api call when rejected
+                await interaction.response.send_message(embed=embed, ephemeral=True)
+                return
+
+
+            # MODAL
+            link = interaction.message.embeds[0].description
+            category = interaction.message.embeds[0].fields[0].value
+            priority = interaction.message.embeds[0].fields[1].value
+            user_note = interaction.message.embeds[0].fields[-1].value
+
+            await interaction.response.send_modal(MyModal(link, category, int(priority), user_note, title="Review Values"))
+
+
 
 
 def setup(bot):
