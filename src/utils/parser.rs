dc294d9af4f47642c4c780d1dfcfce50f78c6b52
holasoyender <javier.projectzafire@gmail.com>
2023-08-23 13:50:15 +0200

    Fix parser inconsistencies

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -3,12 +3,12 @@ use validators::prelude::*;
 use crate::security::sql::check_and_ban;
 
 #[derive(Validator)]
-#[validator(domain(ipv4(Allow), local(Allow), at_least_two_labels(Allow), port(NotAllow)))]
-pub struct DomainWithoutPort(pub String);
+#[validator(domain(ipv4(NotAllow), local(NotAllow), at_least_two_labels(Allow), port(NotAllow)))]
+pub struct Domain(pub String);
 
 pub async fn is_valid_domain(domain: &String, db: &DatabaseConnection, ip: &String) -> bool {
     let domain = domain.replace("http://", "").replace("https://", "");
     let sql_checks = check_and_ban(&domain, db, ip).await;
 
-    DomainWithoutPort::parse_string(domain).is_ok() && !sql_checks
+    Domain::parse_string(domain).is_ok() && !sql_checks
 }
